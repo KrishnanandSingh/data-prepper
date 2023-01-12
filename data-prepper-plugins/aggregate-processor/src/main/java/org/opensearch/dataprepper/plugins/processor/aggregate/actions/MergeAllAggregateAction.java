31ade69dbdeba6fc1a5187ea048a248adedf675f
KrishnanandSingh <krishnanand_singh@cargill.com>
2023-01-12 18:18:26 +0530

    added feature to add fields with data type in config

--- data-prepper-plugins/aggregate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/aggregate/actions/MergeAllAggregateAction.java
+++ data-prepper-plugins/aggregate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/aggregate/actions/MergeAllAggregateAction.java
@@ -6,13 +6,13 @@
 package org.opensearch.dataprepper.plugins.processor.aggregate.actions;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 import org.opensearch.dataprepper.model.annotations.DataPrepperPlugin;
+import org.opensearch.dataprepper.model.annotations.DataPrepperPluginConstructor;
 import org.opensearch.dataprepper.model.event.Event;
 import org.opensearch.dataprepper.model.event.JacksonEvent;
 import org.opensearch.dataprepper.plugins.processor.aggregate.AggregateAction;
@@ -31,16 +31,15 @@ import org.opensearch.dataprepper.plugins.processor.aggregate.GroupState;
  * 
  * @since 1.3
  */
-@DataPrepperPlugin(name = "merge_all", pluginType = AggregateAction.class)
+@DataPrepperPlugin(name = "merge_all", pluginType = AggregateAction.class, pluginConfigurationType = MergeAllAggregateActionConfig.class)
 public class MergeAllAggregateAction implements AggregateAction {
     static final String EVENT_TYPE = "event";
-    public final Map<String, String> dataTypeMap = new HashMap<String, String>();
+    public final Map<String, String> dataTypeMap;
 
-    // @DataPrepperPluginConstructor
-    // public MergeAllAggregateAction(final MergeAllAggregateActionConfig
-    // mergeAllAggregateActionConfig) {
-    // this.dataTypeMap = mergeAllAggregateActionConfig.getDataTypes();
-    // }
+    @DataPrepperPluginConstructor
+    public MergeAllAggregateAction(final MergeAllAggregateActionConfig mergeAllAggregateActionConfig) {
+        this.dataTypeMap = mergeAllAggregateActionConfig.getDataTypes();
+    }
 
     /*
      * (non-Javadoc)
@@ -52,41 +51,16 @@ public class MergeAllAggregateAction implements AggregateAction {
     @Override
     public AggregateActionResponse handleEvent(final Event event, final AggregateActionInput aggregateActionInput) {
         final GroupState groupState = aggregateActionInput.getGroupState();
-        System.out.println(groupState.size());
-        dataTypeMap.put("output_snmp", "integer");
-        dataTypeMap.put("flow_seq_num", "integer");
-        dataTypeMap.put("src_tos", "integer");
-        dataTypeMap.put("input_snmp", "integer");
-        dataTypeMap.put("l4_dst_port", "integer");
-        dataTypeMap.put("tcp_flags", "integer");
-        dataTypeMap.put("in_bytes", "integer");
-        dataTypeMap.put("in_pkts", "integer");
-        dataTypeMap.put("protocol", "integer");
-        dataTypeMap.put("flowset_id", "integer");
-        dataTypeMap.put("version", "integer");
-        dataTypeMap.put("dst_as", "integer");
-        dataTypeMap.put("ip_dscp", "integer");
-        dataTypeMap.put("l4_src_port", "integer");
-        dataTypeMap.put("ipv4_src_addr", "string");
-        dataTypeMap.put("first_switched", "string");
-        dataTypeMap.put("last_switched", "string");
-        dataTypeMap.put("ipv4_dst_addr", "string");
-        
         if (groupState.size() == 0) {
             groupState.putAll(event.toMap());
         }
-        // dataTypeMap.forEach((key, value) -> System.out.println(key + ":" + value));
-        System.out.println();
-       
         Set<String> eventKeys = event.toMap().keySet();
         for (String key : eventKeys) {
-            if (!dataTypeMap.containsKey(key)) {
-                System.out.println("Skipping " + key);
+            if (!this.dataTypeMap.containsKey(key)) {
                 continue;
             }
-            System.out.println("Checking key: " + key);
             Object value = null;
-            String dataType = dataTypeMap.get(key);
+            String dataType = this.dataTypeMap.get(key);
             switch (dataType) {
                 case "integer":
                     value = event.get(key, Integer.class);
@@ -121,7 +95,6 @@ public class MergeAllAggregateAction implements AggregateAction {
                 .withEventType(EVENT_TYPE)
                 .withData(aggregateActionInput.getGroupState())
                 .build();
-        // TODO deduplicate listValues while concluding
         return Optional.of(event);
     }
 }
